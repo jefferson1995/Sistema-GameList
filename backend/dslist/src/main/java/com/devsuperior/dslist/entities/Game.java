1ef40607965be62c6826117d5f045cc7ec36e90c
jefferson barbosa dos santos <jefferson.bds@hotmail.com>
2023-05-09 22:07:33 -0300

    Game base de dados

--- backend/dslist/src/main/java/com/devsuperior/dslist/entities/Game.java
+++ backend/dslist/src/main/java/com/devsuperior/dslist/entities/Game.java
@@ -20,23 +20,38 @@ public class Game {
 	
 	@Column(name = "game_year") //Customizar nome da coluna
 	private Integer year;
+	private String genre;
+	private String platforms;
+	private Double score;
 	private String imgUrl;
+	
+	@Column(columnDefinition = "TEXT")
 	private String shortDescription;
+	
+	@Column(columnDefinition = "TEXT")
 	private String longDescription;
 	
 	public Game() {
 		
 	}
 
-	public Game(Long id, String title, Integer year, String imgUrl, String shortDescription, String longDescription) {
+	
+	
+	public Game(Long id, String title, Integer year, String genre, String platforms, Double score, String imgUrl,
+			String shortDescription, String longDescription) {
 		super();
 		this.id = id;
 		this.title = title;
 		this.year = year;
+		this.genre = genre;
+		this.platforms = platforms;
+		this.score = score;
 		this.imgUrl = imgUrl;
 		this.shortDescription = shortDescription;
 		this.longDescription = longDescription;
 	}
+
+
 
 	public Long getId() {
 		return id;
@@ -61,6 +76,43 @@ public class Game {
 	public void setYear(Integer year) {
 		this.year = year;
 	}
+
+	
+	public String getGenre() {
+		return genre;
+	}
+
+
+
+	public void setGenre(String genre) {
+		this.genre = genre;
+	}
+
+
+
+	public String getPlatforms() {
+		return platforms;
+	}
+
+
+
+	public void setPlatforms(String platforms) {
+		this.platforms = platforms;
+	}
+
+
+
+	public Double getScore() {
+		return score;
+	}
+
+
+
+	public void setScore(Double score) {
+		this.score = score;
+	}
+
+
 
 	public String getImgUrl() {
 		return imgUrl;
