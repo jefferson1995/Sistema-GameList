8ca47bfc097514d00006484a8549adc622c2af01
jefferson barbosa dos santos <jefferson.bds@hotmail.com>
2023-05-09 22:51:18 -0300

    GameMinDTO, GameRepositoy, GameService, GameController

---
+++ backend/dslist/src/main/java/com/devsuperior/dslist/controllers/GameController.java
@@ -0,0 +1,26 @@
+package com.devsuperior.dslist.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.devsuperior.dslist.entities.dto.GameMinDTO;
+import com.devsuperior.dslist.services.GameService;
+
+@RestController
+@RequestMapping(value = "/games")
+public class GameController {
+
+	@Autowired
+	private GameService gameService;
+	
+	@GetMapping
+	public ResponseEntity <List<GameMinDTO>> findAll(){
+		List<GameMinDTO> result = gameService.findAll();		
+		return ResponseEntity.ok().body(result);
+	}
+}
