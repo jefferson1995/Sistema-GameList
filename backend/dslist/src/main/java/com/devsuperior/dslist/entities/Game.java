8efb5becab9d2c7170d75c06c1cd5e22f4534213
jefferson barbosa dos santos <jefferson.bds@hotmail.com>
2023-05-10 21:06:55 -0300

    Modelo de dominío

--- backend/dslist/src/main/java/com/devsuperior/dslist/entities/Game.java
+++ backend/dslist/src/main/java/com/devsuperior/dslist/entities/Game.java
@@ -1,5 +1,6 @@
 package com.devsuperior.dslist.entities;
 
+import java.io.Serializable;
 import java.util.Objects;
 
 import jakarta.persistence.Column;
@@ -11,7 +12,8 @@ import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "tb_game")
-public class Game {
+public class Game implements Serializable{
+	private static final long serialVersionUID = 1L;
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
