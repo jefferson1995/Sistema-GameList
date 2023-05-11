7eb652251723d359448c70033e5ae6991a21482f
jefferson barbosa dos santos <jefferson.bds@hotmail.com>
2023-05-10 22:10:10 -0300

    Get lists

---
+++ backend/dslist/src/main/java/com/devsuperior/dslist/controllers/GameListController.java
@@ -0,0 +1,28 @@
+package com.devsuperior.dslist.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.devsuperior.dslist.entities.dto.GameListDTO;
+import com.devsuperior.dslist.services.GameListService;
+
+@RestController
+@RequestMapping(value = "/lists")
+public class GameListController {
+
+	@Autowired
+	private GameListService gameListService;
+	
+	@GetMapping
+	public ResponseEntity <List<GameListDTO>> findAll(){
+		List<GameListDTO> result = gameListService.findAll();		
+		return ResponseEntity.ok().body(result);
+	}
+	
+	
+}
